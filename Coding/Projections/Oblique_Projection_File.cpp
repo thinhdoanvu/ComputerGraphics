7f37c49f327d16bf731cab9d0b8d58674749d38d
THINH DOAN VU <thinhdv@ntu.edu.vn>
2022-05-30 15:28:13 +0700

    Update Oblique_Projection_File.cpp

--- Coding/Projections/Oblique_Projection_File.cpp
+++ Coding/Projections/Oblique_Projection_File.cpp
@@ -1,198 +1,220 @@
-#include<graphics.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#define maxdinh 20
-//#define FILENAME "chopnhon.inp"
-//#define MTK "chopnhon.mtk"
-#define FILENAME "chopcut.inp"
-#define MTK "chopcut.mtk"
+#include<graphics.h>//line
+#include<stdio.h>//printf, scanf
+#include<stdlib.h>//FILE
+#include<math.h>//sin cos
 
-//khai bao bien
+//#define INPUT "chopnhon.inp"
+//#define MTK "chopnhon.mtk"
+#define INPUT "chopcut.inp"
+#define MTK "chopcut.mtk"
+#define maxdinh 20
+
+int P[maxdinh][maxdinh];//Cac dinh cua vat the
+int D[maxdinh][maxdinh];//ma tran ke
+int R[3][maxdinh];//ma tran ket qua
+int sodinh, sochieu;//so chieu =3 (XYZ)
 int X[maxdinh];
 int Y[maxdinh];
 int Z[maxdinh];
-int P[maxdinh][maxdinh];//ma tran hinh hop chu nhat
-int D[maxdinh][maxdinh];//ma tran ke
-int R[maxdinh][maxdinh];//ma tran ket qua
-float T[maxdinh][3];//ma tran bien doi
-int cot, hang; //hang, cot cua 3D
-int rbd=3;//cot cua ma tran bien doi
-
+float T[3][3];//ma tran bien doi
+int phi;
+float L=0.5;
 FILE *fp;
 
-int phi = 15;
-double L1 = 1;
-double phi_rad = (double) (3.14*phi/180);
-
-//chuong trinh con
-void matranke(){
-	fp=fopen(MTK,"r");
+void nhaptoado(){
+	int i,j;
+	
+	fp=fopen(INPUT,"r");
 	if(fp==NULL){
-		printf("File not found!");
-		exit(1);
+		printf("File not found");
 	}
 	else{
-		int i,j;
-		for(i=0;i<cot;i++){
-			for(j=0;j<cot;j++){
-				fscanf(fp,"%d",&D[i][j]);
-			}
-		}
-		printf("\nMa tran ke cac dinh:\n");
-		for(i=0;i<cot;i++){
-			for(j=0;j<cot;j++){
-				printf("%d  ",D[i][j]);
-			}
-			printf("\n");
-		}
-		fclose(fp);
-	}
-}
-
-void nhaptoado()
-{
-int i,j;
-	fp = fopen(FILENAME,"r");
-	if(fp == NULL){
-		printf("File not found!");
-		exit(1);
-	}
-	else{
-		fscanf(fp,"%d",&hang);
-		fscanf(fp,"%d",&cot);
-		
-		//doc toa do cho P
-		for(i=0;i<hang;i++){
-			for(j=0;j<cot;j++){
+		//doc so chieu
+		fscanf(fp,"%d",&sochieu);
+		//doc so dinh
+		fscanf(fp,"%d",&sodinh);
+		//doc toa do cac dinh cua vat the
+		for(i=0;i<sochieu;i++){
+			for(j=0;j<sodinh;j++){
 				fscanf(fp,"%d",&P[i][j]);
 			}
 		}
 		fclose(fp);
 	}
-	//in ra cac phan tu cua ma tran P
-	printf("\nToa do cac dinh cua hinh chop:\n");
-	for(i=0;i<hang;i++){
-		for(j=0;j<cot;j++){
+
+	//in ra man hinh
+	printf("so chieu khong gian = %d",sochieu);
+	printf("\nso dinh cua vat the = %d",sodinh);
+	printf("\nToa do cac dinh cua vat the la:\n");
+	for(i=0;i<sochieu;i++){
+		for(j=0;j<sodinh;j++){
 			printf("%d  ",P[i][j]);
 		}
 		printf("\n");
-	}			
+	}
 }
 
-void matranbiendoi()
-{
-	T[0][0] = 1; 
-	T[0][1] = 0; 
-	T[0][2] = L1*cos(phi_rad); 
-	T[0][3] = 0;
-	T[1][0] = 0; 
-	T[1][1] = 1; 
-	T[1][2] = L1*sin(phi_rad); 
-	T[1][3] = 0;
-	T[2][0] = 0; 
-	T[2][1] = 0;
-	T[2][2] = 0; 
-	T[2][3] = 0;
-	
+void matranke(){
 	int i,j;
-	printf("\nMa tran bien doi:\n");
-	for(i=0;i<hang;i++){
-		for(j=0;j<3;j++){
-			printf("%.2f ",T[i][j]);
+	fp=fopen(MTK,"r");
+	if(fp==NULL){
+		printf("File not found");
+	}
+	else{
+		for(i=0;i<sodinh;i++){
+			for(j=0;j<sodinh;j++){
+				fscanf(fp,"%d",&D[i][j]);
+			}
+		}
+		fclose(fp);
+	}
+	//in ra ma tran ke
+	printf("\nma tran ke:\n");
+	for(i=0;i<sodinh;i++){
+		for(j=0;j<sodinh;j++){
+			printf("%d  ",D[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-void cabinet()
-{
+void mbiendoiZ(){//theo Z
+	int i,j;
+	T[0][0]=1;
+	T[0][1]=0;
+	T[0][2]=(float)L*cos(phi*3.1416/180);
+	
+	T[1][0]=0;
+	T[1][1]=1;
+	T[1][2]=(float)L*sin(phi*3.1416/180);
+	
+	T[2][0]=0;
+	T[2][1]=0;
+	T[2][2]=0;
+	
+	printf("\nma tran bien doi:\n");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("%.1f  ",T[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void mbiendoiY(){//theo Y
+	int i,j;
+	T[0][0]=1;
+	T[0][1]=(float)L*cos(phi*3.1416/180);
+	T[0][2]=0;
+	
+	T[1][0]=0;
+	T[1][1]=(float)L*sin(phi*3.1416/180);
+	T[1][2]= 1;
+	
+	T[2][0]=0;
+	T[2][1]=0;
+	T[2][2]=0;
+	
+	printf("\nma tran bien doi:\n");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("%.1f  ",T[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void mbiendoiX(){//theo X
+	int i,j;
+	T[0][0]=(float)L*cos(phi*3.1416/180);
+	T[0][1]=1;
+	T[0][2]=0;
+	
+	T[1][0]=(float)L*sin(phi*3.1416/180);
+	T[1][1]=0;
+	T[1][2]= 1;
+	
+	T[2][0]=0;
+	T[2][1]=0;
+	T[2][2]=0;
+	
+	printf("\nma tran bien doi:\n");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("%.1f  ",T[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void oblique(){
 	int i,j,k;
-	  
-	//khoi tao ma tran ket qua 
-	for(i=0; i<hang; i++) //so hang
-	{
-	  	for(k=0; k<cot; k++) //so cot
-	  	{
-			R[i][k] = 0;
+	//khoi tao ma tran ket qua
+	for(i=0;i<sochieu;i++){
+		for(j=0;j<sodinh;j++){
+			R[i][j]=0;
 		}
 	}
-
 	//nhan 2 ma tran
-	for(i=0; i<hang; i++) //so hang P
-	{
-		for(j=0; j<cot; j++)//so cot P
-		{
-			for(k=0; k<rbd; k++)//so cot T
-			{
-				R[i][j] += (float)T[i][k]* P[k][j];
+	for(i=0;i<sochieu;i++){//hang P
+		for(j=0;j<sodinh;j++){//cot P
+			for(k=0;k<3;k++){//cot T
+				R[i][j]+=T[i][k]*P[k][j];
 			}
 		}
 	}
-	
-	//ma tran sau khi bien doi
-	printf("\nToa do cac dinh cua hinh hop sau khi chieu:\n");
-	for(i=0;i<hang;i++)
-	{
-	  	for(j=0;j<cot;j++)
-	  	{
-	  	  	printf("%d  ",R[i][j]);
+	printf("\nMa tran ket qua:\n");
+	for(i=0;i<sochieu;i++){
+		for(j=0;j<sodinh;j++){
+			printf("%d  ",R[i][j]);
 		}
 		printf("\n");
 	}
-}
-
-
-//in hinh chieu ra man hinh
-void inhinhchieu()
-{
-	int i,j;
-	for(i=0; i<cot;i++)
-	{
+	//gan X,Y
+	for(i=0;i<sodinh;i++){
 		X[i]=R[0][i];
 		Y[i]=R[1][i];
-	}
-	//in ra mang X va Y
-	printf("\nToa do XY sau khi bien doi\n");
-	for(i=0;i<cot;i++)
-	{
-		printf("(%d,%d)  ",X[i],Y[i]);
+		Z[i]=R[2][i];
 	}
 }
 
-void oxy()
-{
+void oxy(){//chieu len truc z = chieu dung (z=0)
 	int i,j;
-	  
-	setcolor(RED);
-	for(i=0;i<cot-1;i++)
-	{
-	  	for(j=i+1;j<cot;j++)
-	  	{
-	  	 	if(D[i][j]==1)
-	  	 	{
-	  	 	  	line(X[i],Y[i],X[j],Y[j]);
-	  	 	  	delay(100);
+	for(i=0;i<sodinh-1;i++){
+		for(j=i+1;j<sodinh;j++){
+			if(D[i][j]==1){
+				line(X[i],Y[i],X[j],Y[j]);
 			}
 		}
 	}
 }
 
-//chuong trinh chinh
-int main()
-{
-	//nhap toa do cac dinh cua hinh hop
+int main(){
 	nhaptoado();
-	//nhap ma tran bien doi
-	matranbiendoi();
-	//in ra ma tran ke
 	matranke();
-	//tinh toan ma tran ket qua
-	cabinet();
-	//in ket qua cua phep chieu
-	inhinhchieu();
-	//khoi tao man hinh do hoa
-	initwindow(600,600);
-	oxy();
+	initwindow(800,800);
+	while(1){
+		for(phi=0;phi<360;phi++){
+			mbiendoiZ();
+			oblique();	
+			oxy();		
+			delay(100);
+			cleardevice();
+		}
+		for(phi=0;phi<360;phi++){
+			mbiendoiY();
+			oblique();	
+			oxy();		
+			delay(100);
+			cleardevice();
+		}
+		for(phi=0;phi<360;phi++){
+			mbiendoiX();
+			oblique();	
+			oxy();		
+			delay(100);
+			cleardevice();
+		}
+	}
 	getch();
 }
